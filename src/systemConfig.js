29a1012640ccea6335face3765af2d16c1f1a624
yangchx <7851962+yangchx0513@user.noreply.gitee.com>
2024-09-05 13:18:33 +0800

    init

--- src/systemConfig.js
+++ src/systemConfig.js
@@ -1,4 +1,4 @@
 export const projectName = 'Ubc业务管理系统';
 export const tokenName = 'Authorization';
-export const baseURL = 'http://localhost:8888/ubc/api/v1';
+export const baseURL = 'http://18.224.202.82:8888/ubc/api/v1';
 export const contentType = 'application/json';
